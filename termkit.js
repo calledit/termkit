96942ca7ccd3c6fc7861408e12831566e6d84b1c
Calle Sagulin <calle.sagulin@gant.com>
2014-07-06 23:38:36 +0200

    Js renderer now works but it has flaws just like the tree renderer to get this working we need to go to the webkit source

--- termkit.js
+++ termkit.js
@@ -22,10 +22,15 @@ var StyleConfig = {
 };
 
 var settings = {
-    //HomePage: "https://www.facebook.com/"
+    HomePage: "https://www.facebook.com/"
     //HomePage: "https://news.ycombinator.com/"
-    HomePage: "http://www.w3schools.com/jsref/jsref_indexof_array.asp"
+    //HomePage: "http://www.w3schools.com/jsref/jsref_indexof_array.asp"
     //HomePage: "https://www.webkit.org/blog/116/webcore-rendering-iii-layout-basics/"
+};
+
+
+var termKitState = {
+    ActiveTab: null
 };
 
 
@@ -101,6 +106,11 @@ terminalConverter.getBrowserSize();
 
 var Tabs = [];
 
+screen.key(['C-r'], function(ch, key) {
+    renderTab(Tabs[termKitState.ActiveTab], function(){
+        screen.render();
+    });
+});
 screen.on('resize', function(){
     ViewPort.height = screen.height - TopMenu.height - ConsoleBox.height;
     terminalConverter.getBrowserSize();
@@ -131,11 +141,15 @@ var sysEvents = {
 
 phantom.create({
         binary: "./patched_phantomjs",
+        //binary: "./phantomjs",
         onStderr: function(textData){
-            sysEvents.OnPhantomNotice(textData);
+            sysEvents.OnPhantomNotice('PhantomStdErr: '+textData);
         },
         onStdout: function(textData){
-            sysEvents.OnPhantomNotice(textData);
+            sysEvents.OnPhantomNotice('PhantomStdOut: '+textData);
+        },
+        onExit: function(code, signal){
+            sysEvents.OnPhantomNotice('PhantomExit: Code:'+code+' Signal:'+signal);
         }
     }, function(phant){
     phantomProccess = phant;
@@ -218,11 +232,34 @@ var BrowserActions = {
         //Get A tab from phantom 
         phantomProccess.createPage(function(PTab){
             Tab.PhantomTab = PTab;
+            /*Tab.PhantomTab.set('onConsoleMessage', function(msg, lineNum, sourceId) {
+                document.write(msg)
+                //console.log('CONSOLE: ' + msg);
+                //process.exit(1);
+            }, function(){});
+*/
+            /*Tab.PhantomTab.set('onError', function(msg, trace) {
+
+              var msgStack = ['ERROR: ' + msg];
+
+              if (trace && trace.length) {
+                msgStack.push('TRACE:');
+                trace.forEach(function(t) {
+                  msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
+                });
+              }
+
+              //console.error(msgStack.join('\n'));
+                //process.exit(1);
+
+            });*/
             
             Tab.PhantomTab.set('viewportSize', terminalConverter.browserSize);
             
             Tabs.push(Tab);
             Tab._id = Tabs.length - 1;
+            
+            termKitState.ActiveTab = Tab._id;
 
             Tab.BarUrlInput.setValue(url);
             function OnUrlSubmit(){
@@ -281,7 +318,12 @@ function renderTab(Tab, OnDone){
             }
         }
     },function(){});
-    
+    TREErender(Tab, OnDone);
+    //JSrender(Tab, OnDone);
+}
+
+//Renders the page based on the focusedFrameRenderTreeDump
+function TREErender(Tab, OnDone){
     var testRet = Tab.PhantomTab.get('focusedFrameRenderTreeDump', function(dumpText){
         render_parser(dumpText, {color: StyleConfig.DefaultTabFgColor, bgcolor: StyleConfig.DefaultTabBgColor}, function(Element, PageDefaultColorValues){
             if(Element.ElemType == 'BODY'){
@@ -325,7 +367,161 @@ function renderTab(Tab, OnDone){
         });
         OnDone();
     });   
+
 }
+
+//Renders the page based on info from getClientRects
+function JSrender(Tab, OnDone){
+    Tab.PhantomTab.evaluate(function() {
+        function allNodes(Node, StyleValuesOrg){
+          var RetObj = {type: Node.nodeName, children: []};
+          var BgImage = false;
+          if(!StyleValuesOrg){
+            StyleValuesOrg = {};
+          }
+          var StyleValues = {};
+          for(var styleKey in StyleValuesOrg){
+            StyleValues[styleKey] = StyleValuesOrg[styleKey];
+          }
+          var IntrestingBox = false;
+          if(Node.style){
+            Cstyle =  window.getComputedStyle(Node, null);
+            if(Cstyle.backgroundColor != 'rgba(0, 0, 0, 0)'){
+              IntrestingBox = true;
+              StyleValues.backgroundColor = Cstyle.backgroundColor;
+            }
+            if(Cstyle.backgroundImage != 'none'){
+              IntrestingBox = true;
+              BgImage = Cstyle.backgroundImage;
+            }
+            if(Cstyle.backgroundColor == 'rgba(0, 0, 0, 0)' && BgImage){
+              delete StyleValues.backgroundColor;
+            }
+            StyleValues.color = Cstyle.color;
+          }
+          if(IntrestingBox || Node.nodeType == 3){
+            var Rects = [];
+            if(IntrestingBox){
+                var Rect = Node.getBoundingClientRect();
+                if(Rect && !(Rect.left == 0 && Rect.top == 0 && Rect.right == 0 && Rect.bottom == 0)){
+                    Rects = [Rect]
+                }
+            }else{
+                var range = document.createRange();
+                range.selectNode(Node)
+                Rects = range.getClientRects();
+            }
+            if(Rects.length > 0){
+              RetObj.rects = Rects;
+              RetObj.color = StyleValues.color;
+              if(StyleValues.backgroundColor){
+                RetObj.backgroundColor = StyleValues.backgroundColor;
+              }
+              if(BgImage){
+                RetObj.backgroundImage = BgImage;
+              }
+              if(Node.nodeType == 3){
+                RetObj.text = Node.nodeValue
+              }
+            }
+          }
+          for(var num=0; num < Node.childNodes.length; num++){
+            var Chd = allNodes(Node.childNodes.item(num), StyleValues)
+            if(Chd){
+              RetObj.children.push(Chd);
+            }
+          }
+          if(!RetObj.rects && RetObj.children.length == 0){
+            return(false);
+          }
+          return(RetObj);
+        }
+
+        return(allNodes(this.document));
+    },function(NodeTree){
+        //dump(NodeTree);
+        
+        walkJsNodeTree(NodeTree, function(Node, FromOwner){
+            //console.log(Node.type, Node.text , Node.backgroundColor, Node.backgroundImage, Node.color, Node.rects);
+            if(Node.rects){
+                ElPos = [Node.rects[0].left, Node.rects[0].top]
+                ElSize = [Node.rects[0].width, Node.rects[0].height]
+                var TermPos = terminalConverter.getTerminalPos(ElPos, ElSize);
+                if(TermPos !== false){
+
+                    var StyleObj = {};
+                    if(Node.backgroundColor){
+                        StyleObj.bg = GetRGB(Node.backgroundColor);
+                    }else if(FromOwner.LastBgColor){//If the ower has an image has background we wont get anything so we just use the last color we saw
+                        StyleObj.bg = FromOwner.LastBgColor;
+                    }
+                    if(typeof(FromOwner.Counter) == 'undefined'){
+                        FromOwner.Counter = 0;
+                    }else{
+                        FromOwner.Counter++;
+                    }
+                    
+                    if(Node.backgroundImage){//Somehow i need to convert to image to a usable format
+                        //For now letts just hope the image is some kind of
+                        //filter like a gradient that contains a rgb value if
+                        //not we ignore the image
+                        var RgVal = GetRGB(Node.backgroundImage, 'rgb');
+                        if(RgVal){
+                            StyleObj.bg = RgVal;
+                        }
+                        
+                    }
+                    if(Node.color){
+                        StyleObj.fg = GetRGB(Node.color);
+                    }
+                    if(StyleObj.bg){
+                        FromOwner.LastBgColor = StyleObj.bg;
+                    }
+                    box = blessed.box({
+                        parent: Tab.ViewPort,
+                        left: TermPos.left,
+                        top: TermPos.top,
+                        width: TermPos.width,
+                        height: TermPos.height,
+                        style: StyleObj
+                    });
+                    if(Node.text){
+                        box.setText(Node.text);
+                    }
+                    //box.setText(StrObj(FromOwner.LastBgColor));
+                    //box.setText(Node.type+"_"+FromOwner.Counter);
+                }
+            }
+            return(FromOwner);
+        });
+        OnDone();
+    });
+}
+function walkJsNodeTree(NodeTree, callback, FromOwner){
+    if(typeof(FromOwner) == 'undefined'){
+        var FromOwner = {};
+    }
+    FromOwner = callback(NodeTree, clone(FromOwner));
+    for(var childnum in NodeTree.children){
+        walkJsNodeTree(NodeTree.children[childnum], callback, FromOwner);
+    }
+}
+
+function GetRGB(ColorText, type){
+    if(typeof(type) == 'undefined'){
+        type = '';
+    }
+    Pars = ColorText.split(type+'(');
+    if(Pars.length != 2){
+        return(false);
+    }
+    ColorParts = Pars[1].split(')')[0];
+    value = ColorParts.split(',');
+    if(value[3] == 0){
+        return(false);
+    }
+    return(value);
+I}
 
 function StrObj(Obj){
     return(JSON.stringify(Obj));
@@ -338,3 +534,53 @@ function UrlClean(url){
 
 screen.render();
 
+
+function clone(obj) {
+    if (null == obj || "object" != typeof obj) return obj;
+    var copy = obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+    }
+    return copy;
+}
+
+function PreDump(S, cache, cacheInfo, road){
+    if(typeof(cache) == 'undefined'){
+        cache = [];
+        cacheInfo = [];
+        road = [];
+    }
+    var Dp = {};
+    for(var key in S){
+        road.push(key);
+        var roadJoin = road.join('.');
+        var cacheid = cache.indexOf(S[key]);
+        if (cacheid == -1) {
+            if(typeof(S[key]) == 'object' && S[key] !== null){
+                cacheInfo.push([roadJoin]);
+                cache.push(S[key]);
+                Dp[key] = PreDump(S[key], cache, cacheInfo, road);
+            }else if(typeof(S[key]) === 'function'){
+                var TmDm = PreDump(S[key], cache, cacheInfo, road);
+                var FCont = S[key].toString().replace("\n    [native code]\n", "[native code]")
+                if(TmDm.length == 0){
+                    Dp[key] = {};
+                    Dp[key][FCont] = TmDm;
+                }else{
+                    Dp[key] = FCont;
+                }
+            }else{
+                Dp[key] = S[key];
+            }
+        }else{
+            Dp[key] = "Cyclic previosly Found in: " + cacheInfo[cacheid].join(',');
+            cacheInfo[cacheid].push(roadJoin);
+        }
+        road.pop(key);
+    }
+    return(Dp);
+}
+
+function dump(In){
+    console.log(JSON.stringify(PreDump(In), null, 4));
+}
