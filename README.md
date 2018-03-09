11dfb7682e8e492b282b2ea7aeb0dc9b34d86dcb
Calle Sg <calle@sagulin.se>
2018-03-09 10:35:05 +0100

    Started chrome interface rewrite

--- README.md
+++ README.md
@@ -1,29 +1,27 @@
+# New rewrite in progress
+Old version was based on phantomjs but now as chrome-remote-interface has
+matured it has become ai viable option
 # WebKit based browser for the terminal.
-Based on nodejs, phantomjs, and blessed
+Based on nodejs, chrome-remote-interfac, and blessed
 
 To enter a new url press the urlbar with our mouse
 scroll with your mouse scroll wheel
 
 ## Status
 
-The browser is functional forms are not yeat implemented
-
-### Switch to a more solid base
-It would be preferable if termkit integrated directly with the layout engine.
-Instead of throgh phantomjs as it is right now. That might take some more
-extensive work.
- 
+rewrite in progress
 
 
-![Facebook screenshot](/misc/Facebook.png)
-
-![Github screenshot](/misc/Github.png)
+![Github screenshot](/misc/Github_rewrite.png)
 
 ## Install
 ```bash
 git clone https://github.com/callesg/termkit.git
 cd termkit
-#Clone a copy of phantom js patch and build it
+#Get dependencys
 ./build.sh
+
+#start browser
+node termkit.js
 ```
 
--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@
 
 #Install js modules
 #the master branch of blessed has some horible bugs so we use git directly to get a fork with out those bugs. When resolved use: npm install phantom blessed
-npm install phantom wcwidth
+npm install chrome-remote-interface
 NpmResult=$?
-if [ "${NpmResult}" != "0" ] 
+if [ "${NpmResult}" != "0" ]
 then
 	echo "npm failed. Do you have npm installed? Are our nodejs version to old? termkit is developed on latest stable nodejs"
 	echo "A posible solution may be: sudo apt-get install nodejs npm"
@@ -21,19 +21,3 @@ fi
 cd node_modules
 git clone https://github.com/callesg/blessed
 cd ..
-
-
-#Then get a copy of phantom js
-cd ..
-git clone https://github.com/ariya/phantomjs.git
-cd phantomjs
-#Development is on some stable version of phantomjs
-git checkout d10b8dc5832797be434f43fa2cbd4f1110d035fb
-#Apply patch to phantomjs and webkit
-git apply ../termkit/renderTreeDump.patch
-#build phantomjs get it a bit more silent and speed up the compiling by running in paralel 32 threds is fine as the build is IO bound not cpu
-./build.sh --confirm --jobs 32
-
-#Move the binary in to place
-mv bin/phantomjs ../termkit/patched_phantomjs
-
